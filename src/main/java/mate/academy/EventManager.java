a0b052b81ba677617fc369cd65e3984f3c369960
smagles <lesialist@icloud.com>
2026-01-21 15:13:59 +0100

    Implement a thread-safe asynchronous event manager

--- src/main/java/mate/academy/EventManager.java
+++ src/main/java/mate/academy/EventManager.java
@@ -1,19 +1,30 @@
 package mate.academy;
 
-public class EventManager {
-    public void registerListener(EventListener listener) {
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.LinkedBlockingQueue;
 
+public class EventManager {
+    LinkedBlockingQueue<EventListener> listeners = new LinkedBlockingQueue<>();
+    ExecutorService executorService = Executors.newCachedThreadPool();
+
+    public void registerListener(EventListener listener) {
+        listeners.offer(listener);
     }
 
     public void deregisterListener(EventListener listener) {
-
+        listeners.remove(listener);
     }
 
     public void notifyEvent(Event event) {
-
+        for (EventListener listener : listeners) {
+            executorService.execute(() -> {
+                listener.onEvent(event);
+            });
+        }
     }
 
     public void shutdown() {
-
+        executorService.shutdown();
     }
 }
